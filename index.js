dfd6b7d4cc58e1fba2db4361e5be5c9417f96734
MartinMatousek <martinmatousek5@gmail.com>
2023-04-15 15:18:04 +0200

    add json file as a persistent data storage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
+const fs = require("fs");
 
 // parse application/json
 app.use(bodyParser.json())
@@ -19,14 +20,20 @@ app.post('/vote', (req, res) => {
   console.log(req.ip);
   const ip_address = req.ip;
   const option = req.body.option;
-  
+
+  let votesjson = fs.readFileSync("votes.json");
+  let votes = JSON.parse(votesjson);
   // Check if the user has already voted
-  if (ip_address in votes) {
-    return res.status(400).send('Error: You have already voted');
+  for (var i in votes) {
+    if (votes[i]["ip_address"] == ip_address) {
+      return res.status(400).send('Error: You have already voted');
+    }
   }
   
   // Record the vote
-  votes[ip_address] = option;
+  votes.push({ option, ip_address });
+  votesjson = JSON.stringify(votes);
+  fs.writeFileSync("votes.json", votesjson);
   
   return res.send('Success: Vote recorded');
 });
@@ -37,8 +44,12 @@ app.get('/results', (req, res) => {
   counts['cucumber'] = 0;
   counts['cactus'] = 0;
   counts['idk'] = 0;
-  for (var ip in votes) {
-    counts[votes[ip]]++;
+
+  let votesjson = fs.readFileSync("votes.json");
+  let votes = JSON.parse(votesjson);
+
+  for (var i in votes) {
+    counts[votes[i]["option"]]++;
   }
   return res.send(counts);
 });
